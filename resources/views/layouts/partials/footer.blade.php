5b8e98124d16f42fe93d21f955a602ed7796e930
diahparamitha <diahparamitha01@gmail.com>
2023-09-12 12:38:18 +0700

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center">
+       Copyright &copy; 2021 by <a href="#" class="text-success">Inti-Grafika</a>. Developed by <a href="{{route('teams')}}" class="text-success">IT Teams</a>.
+</footer>
